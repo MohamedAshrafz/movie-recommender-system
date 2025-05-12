0fdd5098b738bc638b40bfe868e5953c5be757f9
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2025-05-12 13:54:10 +0300

    initial commit

---
+++ src/test/java/io/spring/basic/movie_recommender_system/MovieRecommenderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.basic.movie_recommender_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
