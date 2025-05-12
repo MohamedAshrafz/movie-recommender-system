d7c3e53a830365fc6ece829d3dde9576d93ab66e
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2025-05-12 23:07:31 +0300

    Adding primary as commented

--- src/main/java/io/spring/basic/movie_recommender_system/lesson3/filters/CollaborativeFilter.java
+++ src/main/java/io/spring/basic/movie_recommender_system/lesson3/filters/CollaborativeFilter.java
@@ -1,10 +1,12 @@
 package io.spring.basic.movie_recommender_system.lesson3.filters;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component(MovieFiltersNames.COLLABORATIVE_FILTER_BEAN_NAME)
+//@Primary
 public class CollaborativeFilter implements MoviesFilter {
     public List<String> getRecommendations(String movie) {
         return List.of("Finding Nemo", "Ice Age", "Toy Story");
